4b2da76f3b559f5eb74aef5201d8a210eba96952
Zeta0x <94939500+Zeta0x@users.noreply.github.com>
2022-01-14 06:05:06 +0800

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -1,30 +1,29 @@
 import requests
 import json
-import colorama
 from dictor import dictor
-from colorama import Fore
 
-colorama.init(autoreset = True)
+
 def menu():
-    print(f"{Fore.GREEN} +------------------+")
-    print(f"{Fore.GREEN} | BIN Checker v1.0 |")
-    print(f"{Fore.GREEN} | Author : Zeta    |") 
-    print(f"{Fore.GREEN} +------------------+\n")
+    print("+------------------+")
+    print("| BIN Checker v1.0 |")
+    print("| Author : Zeta    |") 
+    print("+------------------+\n")
 menu()
 while True:
 
     def info():
         print()
-        print (f"{Fore.GREEN}[•] Network: " + str.capitalize(scheme))
-        print (f"{Fore.GREEN}[•] Type : " + str.capitalize(types))
-        print (f"{Fore.GREEN}[•] Brand : " + brand)
-        print (f"{Fore.GREEN}[•] Prepaid : " + prepaid)
-        print (f"{Fore.GREEN}[•] Bank : " + bank)
-        print (f"{Fore.GREEN}[•] Bank Phone Number : " + phone)
-        print (f"{Fore.GREEN}[•] Country : " + country)
+        print ("[•] Network: " + str.capitalize(scheme))
+        print ("[•] Type : " + str.capitalize(types))
+        print ("[•] Brand : " + brand)
+        print ("[•] Prepaid : " + prepaid)
+        print ("[•] Bank : " + bank)
+        print ("[•] Bank Phone Number : " + phone)
+        print ("[•] Country : " + country)
 
     while True:
         a = str(input("Enter your BIN: "))
+        print()
         b = a[:7]
         c = b.replace(" ", "")
         first = a[0]
@@ -32,7 +31,7 @@ while True:
         if (len(c) >= 6) and first in check and (len(c) <= 16):
             break
         else:
-            print(f"\n{Fore.RED}Invalid BIN length[" + str(len(c)) + "] or Invalid Format, Please try again! \n")
+            print("Invalid BIN length[" + str(len(c)) + "] or Invalid Format, Please try again! \n")
 
     url = ('https://lookup.binlist.net/' + (c))
 
