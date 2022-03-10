7bc65c04c605f641272638b56b6074d6efb77c83
Zeta0x <94939500+Zeta0x@users.noreply.github.com>
2022-03-10 17:36:55 +0800

    Update lookup.py

--- lookup.py
+++ lookup.py
@@ -3,12 +3,16 @@ import json
 from dictor import dictor
 
 
-def menu():
+def author():
     print("+------------------+")
     print("| BIN Checker v1.0 |")
     print("| Author : Zeta    |") 
-    print("+------------------+\n")
-menu()
+    print("+------------------+")
+    print()
+
+if __name__ == "__main__":
+    author()
+
 while True:
 
     def info():
@@ -22,21 +26,26 @@ while True:
         print ("[•] Country : " + country)
 
     while True:
-        a = str(input("Enter your BIN: "))
-        print()
-        b = a[:7]
-        c = b.replace(" ", "")
-        first = a[0]
-        check = ['3','4','5','6']
-        if (len(c) >= 6) and first in check and (len(c) <= 16):
+        try:
+            a = str(input("Enter your BIN: "))
+            print()
+            b = a[:7]
+            c = b.replace(" ", "")
+            first = a[0]
+            check = ['3','4','5','6']
+            if (len(c) >= 6) and first in check:
+                break
+            else:
+                print("Invalid Format, Please try again! \n")
+        except IndexError:
             break
-        else:
-            print("Invalid BIN length[" + str(len(c)) + "] or Invalid Format, Please try again! \n")
 
-    url = ('https://lookup.binlist.net/' + (c))
-
-    result = requests.get(url)
-    data = json.loads(result.text)
+    url = f"https://lookup.binlist.net/{c}"
+    try:
+        result = requests.get(url)
+        data = json.loads(result.text)
+    except json.decoder.JSONDecodeError:
+        break
 
     scheme = (str(dictor(data, 'scheme', default='Unknown')))
     types = (str(dictor(data, 'type', default='Unknown')))
@@ -45,6 +54,7 @@ while True:
     bank = (str(dictor(data, 'bank.name', default='Unknown')))
     phone = (str(dictor(data,'bank.phone', default='Unknown')))
     country = (str(dictor(data,'country.name', default='Unknown')))
-        
-    info()
+    
+    if __name__ == "__main__":
+        info()
     print()
