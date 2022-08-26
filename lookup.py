65807728d493437b314c377509652fce92ad7303
Zeta0x <94939500+Zeta0x@users.noreply.github.com>
2022-08-26 15:35:18 +0800

    Removed dictor from

--- lookup.py
+++ lookup.py
@@ -1,58 +1,70 @@
 import requests
 import json
-from dictor import dictor
 
 
 print("+------------------+")
-print("| BIN Checker v1.0 |")
+print("| BIN Checker v1.1 |")
 print("| Author : Zeta    |") 
-print("+------------------+")
-print()
+print("+------------------+\n")
 
 
-def info():
+def fetch_info(data):
+    return_data_list =[]
+    req_bin_data = ['scheme', 'type', 'brand', 'prepaid']
+    for i in req_bin_data:
+        try:
+            return_data_list.append(data[i])
+        except KeyError:
+            return_data_list.append('unknown')
+    try:
+        return_data_list.append(data['bank']['name'])
+    except KeyError:
+        return_data_list.append('Unknown')
+        
+    try:
+        return_data_list.append(data['bank']['phone'])
+    except KeyError:
+        return_data_list.append('Unknown')
+        
+    try:
+        return_data_list.append(data['country']['name'])
+    except KeyError:
+        return_data_list.append('Unknown')
+        
+    return return_data_list
+
+
+def print_info(data):
+    print_list = ['[•] Network:', '[•] Type :', '[•] Brand :', '[•] Prepaid :', '[•] Bank :', '[•] Bank phone number :', '[•] Country :']
+    for a, b in zip(print_list, data ):
+        try:
+            print(f'{a} {b.capitalize()}')
+        except AttributeError:
+            print(f'{a} {b}')
     print()
-    print (f"[•] Network: {str.capitalize(scheme)}")
-    print (f"[•] Type : {str.capitalize(types)}")
-    print (f"[•] Brand : {brand}")
-    print (f"[•] Prepaid : {prepaid}")
-    print (f"[•] Bank : {bank}")
-    print (f"[•] Bank Phone Number : {phone}")
-    print (f"[•] Country : {country}")
-
 
 while True:
-
-    while True:
-        try:
-            bin_ = str(input("Enter your BIN: "))
-            bin_number = bin_[:7]
-            card_prefix = bin_[0]
-            valid_prefix = ["3","4","5","6"]
-            if (len(bin_number) >= 6) and card_prefix in valid_prefix:
-                break
-            else:
-                print("\nInvalid Format, please try again!\n")
-        except IndexError:
-            break
-
-    url = f"https://lookup.binlist.net/{bin_number}"
-
     try:
-        result = requests.get(url)
-        data = json.loads(result.text)
-    except json.decoder.JSONDecodeError:
-        break
+        bin_ = str(input("Enter your BIN: "))
+        bin_ = "".join(bin_.split())
+        bin_number = bin_[:7]
+        card_prefix = bin_[0]
+        valid_prefix = ["3","4","5","6"]
+        if (len(bin_number) >= 6):
+            if card_prefix in valid_prefix:
+                url = f"https://lookup.binlist.net/{bin_number}"
+                try:
+                    result = requests.get(url).text
+                    data = json.loads(result)
+                    fetch_list_data = fetch_info(data)
+                    if __name__ == '__main__':
+                        print_info(fetch_list_data)
 
-    scheme = (str(dictor(data, "scheme", default="Unknown")))
-    types = (str(dictor(data, "type", default="Unknown")))
-    brand = (str(dictor(data, "brand", default="Unknown")))
-    prepaid = (str(dictor(data, "prepaid", default="Unknown")))
-    bank = (str(dictor(data, "bank.name", default="Unknown")))
-    phone = (str(dictor(data,"bank.phone", default="Unknown")))
-    country = (str(dictor(data,"country.name", default="Unknown")))
-
-    if __name__ == "__main__":
-        info()
-
-    print()
+                except json.decoder.JSONDecodeError:
+                    print("Invalid Format, please try again!\n")
+            else:
+                print("Invalid Format, please try again!\n")
+        else:
+            print("Invalid Format, please try again!\n")
+    except IndexError:
+        print("Invalid Format, please try again!\n")
